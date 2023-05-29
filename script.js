9fdc4616ec656c63f067719bb92ca1a735846fb5
Diego Gonçalves <diegobass55@hotmail.com>
2023-05-29 15:44:16 -0300

    alteração para o formato monetario

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ document.getElementById('calcular').addEventListener('click', function() {
     const tempo = parseFloat(document.getElementById('tempo').value);
     const aportes = parseFloat(document.getElementById('aportes').value);
 
-    const tempoAnos = parseFloat(tempo*12);
+    const tempoAnos = parseFloat(tempo * 12);
 
     let totalFinal = value;
     for (let i = 0; i < tempoAnos; i++) {
         totalFinal *= (1 + taxa);
         totalFinal += aportes;
-        
     }
 
-    document.getElementById('totalFinal').innerHTML = "R$" + totalFinal.toFixed(2);
+    const formattedTotal = totalFinal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    document.getElementById('totalFinal').innerHTML = formattedTotal;
 });
