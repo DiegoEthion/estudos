3119f9699986c27abff73640bc5982327ec003e0
Diego Gonçalves <diegobass55@hotmail.com>
2023-06-05 21:32:43 -0300

    adição da possibilidade de alterar taxa e período em anos e meses

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-
 document.getElementById('calcular').addEventListener('click', function() {
     const value = parseFloat(document.getElementById('value').value);
     const taxa = parseFloat(document.getElementById('taxa').value) / 100;
     const tempo = parseFloat(document.getElementById('tempo').value);
     const aportes = parseFloat(document.getElementById('aportes').value);
 
-    const tempoAnos = parseFloat(tempo * 12);
+    const taxaTipo = document.getElementById('taxaTipo').value;
+    const tempoTipo = document.getElementById('tempoTipo').value;
+
+    let tempoCalculado;
+    if (tempoTipo === 'meses') {
+        tempoCalculado = tempo;
+    } else if (tempoTipo === 'anos') {
+        tempoCalculado = tempo * 12;
+    }
+
+    let taxaCalculada;
+    if (taxaTipo === 'meses') {
+        taxaCalculada = taxa;
+    } else if (taxaTipo === 'anos') {
+        taxaCalculada = Math.pow(1 + taxa, 1 / 12) - 1;
+    }
 
     let totalFinal = value;
-    for (let i = 0; i < tempoAnos; i++) {
-        totalFinal *= (1 + taxa);
+    for (let i = 0; i < tempoCalculado; i++) {
+        totalFinal *= (1 + taxaCalculada);
         totalFinal += aportes;
     }
 
